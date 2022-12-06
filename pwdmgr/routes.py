f9072576bf774fdaffa80c5faa411d7a1e744088
Subhrajyoty Roy <subhrajyotyroy@gmail.com>
2022-12-06 09:23:24 +0530

    feat: added login and signup templates

--- pwdmgr/routes.py
+++ pwdmgr/routes.py
@@ -6,6 +6,10 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/auth/login/')
 def login():
     return render_template('login.html')
+
+@app.route('/auth/signup/')
+def signup():
+    return render_template('signup.html')
